676091feb2ad27571769ac77847050c054d4c9a0
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-08 13:28:45 +0000

    A Complete Solution to Ex1

--- Ex1/top.v
+++ Ex1/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #1 
-// Student Name:
-// Date: 
+// Student Name:Victoria Walls
+// Date: 08/06/20
 //
 //  Description: In this exercise, you need to design a logical unit, where the  
 //  output acts according to the following truth table:
@@ -30,7 +30,12 @@ module logicalunit(
     );
     
     wire   out;
-         
-      //Todo: add you logic here
+    wire [1:0]AB;     
+	assign AB = {a,b};
+        assign AB = {a,b};
+        assign out = (AB==2'b11) ? func[3] :	//If AB=11 then takes 3rd least sig bit, AB=10 then 2nd least 							and so on (as in truth table)
+	(AB==2'b10) ? func[2] :
+	(AB==2'b01) ? func[1] :
+	func[0];
 
 endmodule

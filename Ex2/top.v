da0315e88c9ec94915529eb6a5047c9741e084ca
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-08 14:10:13 +0000

    Complete Solution to Ex2

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 
-// Student Name:
-// Date: 
+// Student Name: Victoria Walls
+// Date: 08/06/20
 //
 //  Description: In this exercise, you need to design a multiplexer, where the  
 //  output acts according to the following truth table:
@@ -22,11 +22,28 @@
 
 module mux(
     //Todo: define inputs here
-    );
+    input a,
+    input b,
+    input sel
+    output out
+	);
     
     //Todo: define registers and wires here
+    parameter CLK_PERIOD = 10;
+    	reg  clk, out;
 
+    initial begin 
+	  clk=0;   
+	forever
+	#(CLK_PERIOD/2) 
+	clk=~clk;		//negate clk so it is a fall delay
+	end
 
     //Todo: define your logic here                 
-      
+     always @(posedge clk)
+	if(sel)
+	out = b;
+	else
+	out = a ;         
+
 endmodule

24dfe7cc0ca32b00dadb8408d5d52660f230c345
Cloud User <centos@ip-172-31-4-48.eu-west-2.compute.internal>
2020-06-09 13:19:12 +0000

    Solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name:Victoria Walls
+// Date: 09/06/20
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -16,3 +16,29 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+ module dice(
+   
+ //Todo: define inputs here
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0] throw
+    );
+
+//logic
+    always @(posedge clk) begin
+
+    	if (rst)
+    	throw <= 3'b000;
+    	else begin
+    	if ((throw == 3'b000) | (throw == 3'b111))
+    	throw <= 3'b001;
+    	if (throw < 3'b110)
+    	throw <= throw + button;
+    	else
+    	throw <= button ? 3'b001 : throw;
+    	end
+    end
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,91 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+//Todo: Parameters
+    parameter CLK_PERIOD = 10;
+
+//Todo: Regitsers and wires
+    reg clk;
+    reg button;
+    reg rst;
+    reg err;
+    wire [2:0] throw;
+    reg [2:0] throw_prev;
+
+//Todo: Clock generation
+    initial
+    begin
+    clk = 1'b0;
+    forever
+    #(CLK_PERIOD/2) clk=!clk;
+    end
+
+//Todo: User logic
+	initial begin
+       	err=0;
+       	button=1;
+	rst = 1;
+    	clk = 0;
+	#10
+	rst = 0;
+       forever begin
+
+       #10
+
+       if (rst&(throw!=0))		//is reset=1 and throw is not 0 then there is an error
+       begin
+       $display("***TEST FAILED! Didn't reset! throw==%d, throw_prev==%d, rst='%d'***", throw,throw_prev,rst);
+       err=1;
+       end
+
+
+if ((throw == 0) | (throw == 3'b111) &(rst!=0))	//if throw=0 or 111 and the reset is not 0 then there is an error (since reset is the only way to get these)
+         begin
+         $display("***TEST FAILED! Out of bounds! throw==%d, throw_prev==%d, reset='%d' ***",throw,throw_prev,rst);
+	err = 1;
+	end
+
+else begin
+if (throw_prev != 3'b110) begin
+if ((button&(throw!=throw_prev+1))| (!button&(throw!=throw_prev)))
+         begin
+         $display("***TEST FAILED! Button doesn't work! throw==%d, throw_prev==%d, button='%d' ***",throw,throw_prev,button);
+	err = 1;
+        end
+end
+	else begin
+	if ((button&(throw!=3'b001))| (!button&(throw!=throw_prev)))
+         begin
+        $display("***TEST FAILED! Button doesn't work for 6! throw==%d, throw_prev==%d, button='%d' ***",throw,throw_prev,button);
+	err = 1;
+        end
+end
+end
+
+	throw_prev = throw;
+	button =!button;
+end
+end
+
+//Todo: Finish test, check for success
+      initial begin
+      #200 
+      if (err==0)
+      $display("***TEST PASSED! :) ***");
+      $finish;
+      end
+
+//Todo: Instantiate counter module
+	dice top (
+	     .clk (clk),
+	     .rst (rst),
+		 .button (button),
+	     .throw (throw)
+	     );
+
+endmodule 
